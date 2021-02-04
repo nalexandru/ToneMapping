ec24c9c15112d8a5f8b53c7b3fd6b878aa710e07
Alexandru Naiman <9295849+nalexandru@users.noreply.github.com>
2021-02-04 13:01:30 +0200

    - Mac OS X support

--- ToneMapping/ToneMapping.cxx
+++ ToneMapping/ToneMapping.cxx
@@ -1,6 +1,16 @@
 #include <math.h>
 
-#include <Windows.h>
+#ifdef _WIN32
+#	define NOMINMAX
+#	include <Windows.h>
+#else
+#	include <pthread.h>
+#	define DWORD void *
+#	define WINAPI
+	// using void * for DWORD is not correct, but pthreads defines the thread
+	// procedure as returning void * so that's why it's like that.
+#endif
+
 #include <xmmintrin.h>
 
 #include "Tonemapping.h"
@@ -11,18 +21,32 @@ struct ThreadArgs
 	uint8_t *ldr;
 	uint32_t count;
 	float exposure, invGamma;
+#ifdef _WIN32
 	HANDLE evt;
+#endif
 };
 
 static uint32_t _cpuCount = 0;
-static HANDLE *_threads = NULL;
 static struct ThreadArgs *_args = NULL;
+
+#ifdef _WIN32
+static HANDLE *_threads = NULL;
+#else
+static pthread_t *_threads = NULL;
+#endif
 
 static inline float
 clamp(float v, float min, float max)
 {
 	_mm_store_ss(&v, _mm_min_ss(_mm_max_ss(_mm_set_ss(v), _mm_set_ss(min)), _mm_set_ss(max)));
 	return v;
+}
+
+static inline float
+max(float f1, float f2)
+{
+	_mm_store_ss(&f1, _mm_max_ss(_mm_set_ss(f1), _mm_set_ss(f2)));
+	return f1;
 }
 
 static DWORD WINAPI
@@ -56,8 +80,10 @@ _acesToneMapProc(void *a)
 		ldr += 3;
 	}
 
+#ifdef _WIN32
 	SetEvent(args.evt);
-
+#endif
+	
 	return 0;
 }
 
@@ -96,8 +122,10 @@ _uc2ToneMapProc(void *a)
 		ldr += 3;
 	}
 
+#ifdef _WIN32
 	SetEvent(args.evt);
-
+#endif
+	
 	return 0;
 }
 
@@ -129,8 +157,10 @@ _hejlRichardToneMapProc(void *a)
 		ldr += 3;
 	}
 
+#ifdef _WIN32
 	SetEvent(args.evt);
-
+#endif
+	
 	return 0;
 }
 
@@ -159,7 +189,9 @@ _reinhardToneMapProc(void *a)
 		ldr += 3;
 	}
 
+#ifdef _WIN32
 	SetEvent(args.evt);
+#endif
 
 	return 0;
 }
@@ -167,6 +199,7 @@ _reinhardToneMapProc(void *a)
 void
 initToneMap(void)
 {
+#ifdef _WIN32
 	SYSTEM_INFO sysInfo;
 	GetSystemInfo(&sysInfo);
 
@@ -177,6 +210,12 @@ initToneMap(void)
 
 	for (uint32_t i = 0; i < _cpuCount; ++i)
 		_args[i].evt = CreateEvent(NULL, FALSE, FALSE, NULL);
+#else
+	_cpuCount = 2;
+	
+	_args = (struct ThreadArgs *)calloc(_cpuCount, sizeof(*_args));
+	_threads = (pthread_t *)calloc(_cpuCount, sizeof(*_threads));
+#endif
 }
 
 void
@@ -196,7 +235,9 @@ toneMap(enum ToneMapMode tm, float *exr, uint8_t *ldr, uint32_t width, uint32_t 
 	default: _acesToneMapProc(&_args[0]); break;
 	}
 
+#ifdef _WIN32
 	ResetEvent(_args[0].evt);
+#endif
 }
 
 void
@@ -207,7 +248,12 @@ toneMapMT(enum ToneMapMode tm, float *hdr, uint8_t *ldr, uint32_t width, uint32_
 
 	const float invGamma = 1.f / gamma;
 
+#ifdef _WIN32
 	LPTHREAD_START_ROUTINE proc = NULL;
+#else
+	void *(*proc)(void *) = NULL;
+#endif
+	
 	switch (tm) {
 	case TM_UC2: proc = _uc2ToneMapProc; break;
 	case TM_HEJL_RICHARD: proc = _hejlRichardToneMapProc; break;
@@ -223,18 +269,31 @@ toneMapMT(enum ToneMapMode tm, float *hdr, uint8_t *ldr, uint32_t width, uint32_
 		_args[i].invGamma = invGamma;
 		_args[i].count = hdrShareSize;
 
+#ifdef _WIN32
 		CreateThread(NULL, 0, proc, &_args[i], 0, NULL);
+#else
+		pthread_create(&_threads[i], NULL, proc, &_args[i]);
+#endif
 	}
 
 	for (uint32_t i = 0; i < _cpuCount; ++i) {
+#ifdef _WIN32
 		WaitForSingleObject(_args[i].evt, INFINITE);
 		CloseHandle(_threads[i]);
+#else
+		pthread_join(_threads[i], NULL);
+#endif
 	}
 }
 
 void
 termToneMap(void)
 {
+#ifdef _WIN32
 	HeapFree(GetProcessHeap(), 0, _threads);
 	HeapFree(GetProcessHeap(), 0, _args);
+#else
+	free(_threads);
+	free(_args);
+#endif
 }
